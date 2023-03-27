cf2e027d8252ac616d20efe806b60ceecc74c3dd
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-03-27 10:54:31 +0000

    update ljf

--- temp/LJF.cpp
+++ temp/LJF.cpp
@@ -24,13 +24,14 @@ void ljf(vector<Process> &processes)
     int current_time = 0;
     int total_waiting_time = 0;
     int total_turnaround_time = 0;
+    int total_completion_time = 0;
 
     // Print header for process details table
     cout << endl
          << "Process Details" << endl;
-    cout << "+-----+--------------+------------+-----------------+-------------------+" << endl;
-    cout << "| PID | Arrival Time | Burst Time | Waiting Time (s) | Turnaround Time (s) |" << endl;
-    cout << "+-----+--------------+------------+-----------------+-------------------+" << endl;
+    cout << "+-----+--------------+------------+-----------------+-------------------+---------------------+" << endl;
+    cout << "| PID | Arrival Time | Burst Time | Waiting Time (s) | Turnaround Time (s) | Completion Time (s) |" << endl;
+    cout << "+-----+--------------+------------+-----------------+-------------------+---------------------+" << endl;
 
     for (int i = 0; i < n; i++)
     {
@@ -39,24 +40,29 @@ void ljf(vector<Process> &processes)
         int end_time = start_time + p.burst_time;
         int waiting_time = max(0, start_time - p.arrival_time);
         int turnaround_time = end_time - p.arrival_time;
+        int completion_time = end_time;
         total_waiting_time += waiting_time;
         total_turnaround_time += turnaround_time;
+        total_completion_time += completion_time;
         current_time = end_time;
 
         // Print process details row
-        cout << "| " << setw(3) << p.id << " | " << setw(12) << p.arrival_time << " | " << setw(10) << p.burst_time << " | " << setw(15) << waiting_time << " | " << setw(17) << turnaround_time << " |" << endl;
+        cout << "| " << setw(3) << p.id << " | " << setw(12) << p.arrival_time << " | " << setw(10) << p.burst_time << " | " << setw(15) << waiting_time << " | " << setw(17) << turnaround_time << " | " << setw(19) << completion_time << " |" << endl;
     }
 
     // Print footer for process details table
-    cout << "+-----+--------------+------------+-----------------+-------------------+" << endl;
+    cout << "+-----+--------------+------------+-----------------+-------------------+---------------------+" << endl;
 
     double avg_waiting_time = static_cast<double>(total_waiting_time) / n;
     double avg_turnaround_time = static_cast<double>(total_turnaround_time) / n;
+    double avg_completion_time = static_cast<double>(total_completion_time) / n;
 
-    // Print average waiting time and turnaround time
+    // Print average waiting time, turnaround time, and completion time
     cout << "Average waiting time: " << fixed << setprecision(2) << avg_waiting_time << endl;
     cout << "Average turnaround time: " << fixed << setprecision(2) << avg_turnaround_time << endl;
+    cout << "Average completion time: " << fixed << setprecision(2) << avg_completion_time << endl;
 }
+
 
 int main()
 {
