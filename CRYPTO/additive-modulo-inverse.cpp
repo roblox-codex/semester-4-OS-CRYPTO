d75cb272de6bc2408589feafac6d559936875971
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-03-27 20:16:58 +0000

    update additive modulo inverse normal

--- CRYPTO/additive-modulo-inverse.cpp
+++ CRYPTO/additive-modulo-inverse.cpp
@@ -2,21 +2,27 @@
 
 using namespace std;
 
-int main () {
+int additiveModInverse(int a, int m) {
+    a = a % m;
+    for (int x = 1; x < m; x++) {
+        if ((a + x * m) % m == 0) {
+            return x;
+        }
+    }
+    return -1; // additive inverse does not exist
+}
+
+int main() {
     int a, m;
-    cout << "Enter a and m: ";
-    cin >> a >> m;
-    int r1 = a, r2 = m, r;
-    while (r2 > 0) {
-        r = r1 % r2;
-        r1 = r2;
-        r2 = r;
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter m: ";
+    cin >> m;
+    int result = additiveModInverse(a, m);
+    if (result == -1) {
+        cout << "Additive inverse does not exist\n";
+    } else {
+        cout << "Additive inverse of " << a << " modulo " << m << " is " << result << "\n";
     }
-    if (r1 != 1) {
-        cout << "No multiplicative inverse exists" << endl;
-        return 0;
-    }
-    int b = (m - a) % m;
-    cout << "Multiplicative inverse of " << a << " modulo " << m << " is " << b << endl;
     return 0;
 }
